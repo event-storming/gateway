15cf8ba33854b0efcdfd9550aad9fcd2f6c4a3e4
kimscott <sanaloveyou@uengine.org>
2019-12-19 10:23:12 +0900

    .

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -2,7 +2,9 @@ package com.example.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -11,7 +13,6 @@ public class Application {
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
     }
-
 
 }
 
